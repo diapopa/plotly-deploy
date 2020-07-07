bfc77769a0e872735412c4a4e58bcb5682f2a92b
diapopa <dia.popa@hotmail.com>
2020-07-06 23:04:31 -0400

    Added files.

--- plots.js
+++ plots.js
@@ -35,55 +35,46 @@ function buildMetadata(sample) {
 }
 
 
-function buildCharts(idVal) {
-    d3.json("samples.json").then((data) => {
-        var samplesFromData = data.samples;
-        //console.log(samplesFromData)
-        var resultArray = samplesFromData.filter(sampleObj => sampleObj.id == idVal);
-        
-        //console.log(result)
-    
-        // Sort the cities by growth. Can only sort an array not a dictionary. 
-        var sortedBacteria = resultArray.sort((a,b) =>
-        a.sample_values - b.sample_values).reverse(); 
-
-        //Grab the first value in the sorted array
-        //var resultSorted = sortedBacteria[0];
-        //console.log(resultSorted);
-
-        // Select top five cities by population growth
-        var topTenOtuLabels = sortedBacteria.slice(0,10);
-        console.log("top ten vals")
-        //console.log(topTenOtuLabels[0]);
-
-        // Create arrays for top five city names and top five growth figures
-        var topTenBacteriaNames = topTenOtuLabels.map(bacteria => bacteria.otu_labels);
-        //var topTenBacteriaValues = topTenOtuLabels.map(bacteria => parseInt(bacteria.sample_values));
-        var topTenBacteriaValues = []
-        var i
-        for (i=0;i < 5; i++) {
-            topTenBacteriaValues.append(sortedBacteria.sample_values[i]);
-        }
-
-        console.log(topTenBacteriaNames);
-        console.log(topTenBacteriaValues);
-        
-        // Create bar chart
-        var trace = {
-            x: topTenBacteriaNames,
-            y: topTenBacteriaValues,
-            type: "bar",
-            orientation: "h"
-            };
-        var data = [trace];
-        var layout = {
-            title: "title",
-            xaxis: { title: "City" },
-            yaxis: { title: "Population Growth, 2016-2017"}
-        };
-        Plotly.newPlot("bar-plot", data, layout);
-        })
-}
+function buildCharts(idVal) {
+        d3.json("samples.json").then((data) => {
+            var samplesFromData = data.samples;
+            //console.log(samplesFromData)
+            var resultArray = samplesFromData.filter(sampleObj => sampleObj.id == idVal);
+            
+            //console.log(result)
+        
+            // Sort the cities by growth. Can only sort an array not a dictionary. 
+            var sortedBacteria = resultArray.sort((a,b) =>
+            a.sample_values - b.sample_values).reverse(); 
+            //Grab the first value in the sorted array
+            //var resultSorted = sortedBacteria[0];
+            //console.log(resultSorted);
+            // Select top five cities by population growth
+            var topTenOtuLabels = sortedBacteria.slice(0,10);
+            console.log("top ten vals")
+            //console.log(topTenOtuLabels[0]);
+            // Create arrays for top five city names and top five growth figures
+            var topTenBacteriaNames = topTenOtuLabels[0].map(bacteria => bacteria.otu_labels);
+            var topTenBacteriaValues = topTenOtuLabels[0].map(bacteria => parseInt(bacteria.sample_values));
+            console.log(topTenBacteriaNames);
+            console.log(topTenBacteriaValues);
+            
+            // Create bar chart
+            var trace = {
+                x: topTenBacteriaNames,
+                y: topTenBacteriaValues,
+                type: "bar",
+                orientation: "h"
+                };
+            var data = [trace];
+            var layout = {
+                title: "title",
+                xaxis: { title: "City" },
+                yaxis: { title: "Population Growth, 2016-2017"}
+            };
+            Plotly.newPlot("bar-plot", data, layout);
+            })
+    }
 
 function optionChanged(newSample) {
     buildMetadata(newSample);
