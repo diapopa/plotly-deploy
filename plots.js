130ab9e5f839b0bd7c715d895eb47510be038e01
diapopa <dia.popa@hotmail.com>
2020-07-08 18:14:34 -0400

    Added files.

--- plots.js
+++ plots.js
@@ -63,8 +63,8 @@ function buildCharts(idVal) {
             
             // Create bar chart
             var trace = {
-                y: topTenBacteriaNames,
                 x: topTenBacteriaValues,
+                y: topTenBacteriaNames,
                 type: "bar",
                 orientation: "h"
                 };
@@ -73,7 +73,33 @@ function buildCharts(idVal) {
                 text :[topTenOtuLabels]
             };
             Plotly.newPlot("bar-plot", data, layout);
+
+            // Create bubble chart
+            var trace1 = {
+                x: topTenBacteriaValues,
+                y: topTenBacteriaNames,
+                mode: 'markers',
+                marker: {
+                  color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+                  opacity: [1, 0.8, 0.6, 0.4],
+                  size: [40, 60, 80, 100]
+                }
+              };
+              
+              var data = [trace1];
+              
+              var layout = {
+                title: 'Marker Size and Color',
+                showlegend: false,
+                height: 600,
+                width: 600
+              };
+              
+              Plotly.newPlot('myDiv', data, layout);
+
             })
+
+
     }
 
 function optionChanged(newSample) {
